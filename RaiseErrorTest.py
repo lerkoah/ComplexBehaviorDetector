18b8449f3bf42805486bcba32d37a55e36df9a00
lerkoah <lerko.araya@ug.uchile.cl>
2017-01-19 10:15:30 -0300

    Change code for testings, add imports

--- RaiseErrorTest.py
+++ RaiseErrorTest.py
@@ -1,47 +1,5 @@
-class BaseDetector(object):
-    def __init__(self):
-        self.dectector = 0
-        self.params = None
-        self.report = 'Reporting alarm'
-    def configure(self,params):
-        self.params = params
-    def execute(self):
-        if self.params == 1:
-            return 0
-        elif self.params == 0:
-            return 0
+from DetectorsType1 import *
 
-    def sendAlarm(self):
-        # print self.report
-        raise Exception(self.report)
-    def executeTruePositive(self):
-        self.params = 0
-    def executeTrueNegative(self):
-        self.params = 1
-
-# SilentTestDetector never detect
-class SilentTestDetector(BaseDetector):
-    def execute(self):
-        BaseDetector.execute(self)
-        return 0
-
-# AlertingTestDetector always detect
-class AlertingTestDetector(BaseDetector):
-    def execute(self):
-        BaseDetector.execute(self)
-        return 1
-# HaltingTestDetector never returns
-class HaltingTestDetector(BaseDetector):
-    def execute(self):
-        BaseDetector.execute(self)
-        while True:
-            pass
-
-# RaiseErrorDetector always fails with code error, returns -1
-class RaiseErrorTestDetector(BaseDetector):
-    def execute(self):
-        BaseDetector.execute(self)
-        return -1
 def main():
     # myDetector = SilentTestDetector()
     # myDetector = AlertingTestDetector()
