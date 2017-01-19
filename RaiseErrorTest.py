266ec92d69f7cb0a1e243896468cf9981bb0ec41
lerkoah <lerko.araya@ug.uchile.cl>
2017-01-19 10:17:33 -0300

    remove some lines

--- RaiseErrorTest.py
+++ RaiseErrorTest.py
@@ -8,12 +8,5 @@ def main():
 
     detection = myDetector.execute()
 
-    if detection == 1:
-        myDetector.sendAlarm()
-    elif detection == -1:
-        raise SystemError('Error code 123')
-
-
-
 if __name__ == '__main__':
     main()
