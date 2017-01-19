7c94fdcd0ab4645ff0af4f4faee52324d6903ea5
lerkoah <lerko.araya@ug.uchile.cl>
2017-01-19 17:03:59 -0300

    delete monit thrash

--- bash/handleAlertTest.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-################################################################
-## This file is used as a handler for run Alerting Test in Monit
-################################################################
-LOG_FILE=/home/lerko/Desktop/Detector/AlertingTest.log
-
-sudo python /home/lerko/Desktop/Detector/AlertingTest.py >> $LOG_FILE
--- bash/handleSilentTest.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-################################################################
-## This file is used as a handler for run Alerting Test in Monit
-################################################################
-LOG_FILE=/home/lerko/Desktop/Detector/SilentTest.log
-
-sudo python /home/lerko/Desktop/Detector/SilentTest.py >> $LOG_FILE
