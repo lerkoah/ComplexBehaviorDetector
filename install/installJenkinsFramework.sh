4192736a62e86c5cc6a6fc2090b6643a6a425437
lerkoah <lerko.araya@ug.uchile.cl>
2017-01-24 10:15:37 -0300

    installJenkinsFramework.sh edited

--- install/installJenkinsFramework.sh
+++ install/installJenkinsFramework.sh
@@ -5,4 +5,12 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 yum install jenkins
 
+python editLinesJenkins.py
+
+echo -n 'Changing propietaries...'
+chown -R root:root /var/lib/jenkins
+chown -R root:root /var/cache/jenkins
+chown -R root:root /var/log/jenkins
+echo 'done.'
+
 exit
