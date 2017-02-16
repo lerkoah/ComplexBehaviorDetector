dd7c45d375b07627fb9b24da83acf5d321889123
lerkoah <lerko.araya@ug.uchile.cl>
2017-02-16 11:20:39 -0300

    AlarmSelector args

--- AlarmSystem/args.py
+++ AlarmSystem/args.py
@@ -1,14 +1,11 @@
 import argparse
-import timeago
 
 def args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', type=str, help='Config file path')
-    parser.add_argument('-n', '--nAlarm', type=int, help='Number of Alarms to generate')
 
     opts = parser.parse_args()
 
     return {
-        'config_file': opts.config,
-        'number_alarms' : opts.nAlarm
+        'config_file': opts.config
     }
